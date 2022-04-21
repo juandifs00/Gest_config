b121789ecfdfd78bfee97240044fb0b8d7877703
juandi <minionpaul85@gmail.com>
2022-04-21 13:24:07 -0500

    Temp_8

--- myapp.py
+++ myapp.py
@@ -1,3 +1,4 @@
+"""Pruebas de FastAPI"""
 import uvicorn
 from fastapi import FastAPI
 
@@ -5,7 +6,8 @@ app = FastAPI()
 
 @app.get("/hi")
 def say_hi():
-  return 'Hola soy Alexis'
+    """Funcion de retorno comentario"""
+    return 'Hola soy Juandi'
 
 if __name__=='__main__':
-  uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
