b2ee658c540927e004c7d7f6b733dcb46f3c37a3
juandi <minionpaul85@gmail.com>
2022-04-21 13:27:33 -0500

    Temp_9

--- myapp_test.py
+++ myapp_test.py
@@ -4,7 +4,7 @@ import myapp
 class TestMyApp(unittest.TestCase):
 
     def test_hi(self):
-      self.assertEquals(myapp.say_hi(),'Hola soy Alexis')
+      self.assertEquals(myapp.say_hi(),'Hola soy Juandi')
 
 if __name__=='__main__':
   unittest.main()
